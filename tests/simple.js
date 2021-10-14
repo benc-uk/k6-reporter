2d56b6bfa184ab195f005c0c2a17b4d4d8b37698
Ben Coleman <benc.uk@gmail.com>
2021-10-14 18:00:18 +0100

    better error handling & debug mode (#18)
    * better error handling & debug mode
    
    * Many custom metric improvements

--- tests/simple.js
+++ tests/simple.js
@@ -2,55 +2,54 @@
 // A generic load test script provided for example purposes and testing
 //
 
-import { check, group, sleep } from "k6";
-import http from "k6/http";
+import { check, group, sleep } from 'k6'
+import http from 'k6/http'
 
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
-//import { htmlReport } from "../dist/bundle.js";
-import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+//import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { htmlReport } from '../dist/bundle.js'
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js'
 
-const TARGET_URL = __ENV.TEST_TARGET || "https://benc.dev";
-const RAMP_TIME = __ENV.RAMP_TIME || "1s";
-const RUN_TIME = __ENV.RUN_TIME || "2s";
-const USER_COUNT = __ENV.USER_COUNT || 20;
-const SLEEP = __ENV.SLEEP || 1;
+const TARGET_URL = __ENV.TEST_TARGET || 'https://benc.dev'
+const RAMP_TIME = __ENV.RAMP_TIME || '1s'
+const RUN_TIME = __ENV.RUN_TIME || '2s'
+const USER_COUNT = __ENV.USER_COUNT || 20
+const SLEEP = __ENV.SLEEP || 0.5
 
 export function handleSummary(data) {
   return {
-    "summary.html": htmlReport(data),
-    stdout: textSummary(data, { indent: " ", enableColors: true }),
-  };
+    'summary.html': htmlReport(data, { debug: false }),
+    stdout: textSummary(data, { indent: ' ', enableColors: true }),
+  }
 }
 
-// Very simple ramp up from zero to VUS_MAX over RAMP_TIME, then runs for further RUN_TIME
 export let options = {
   stages: [
     { duration: RAMP_TIME, target: USER_COUNT },
     { duration: RUN_TIME, target: USER_COUNT },
   ],
   thresholds: {
-    http_req_duration: ["p(95)<1000"],
-    iteration_duration: ["max<4000"],
+    http_req_duration: ['p(95)<1000'],
+    iteration_duration: ['max<4000'],
   },
-};
+}
 
 export default function () {
-  let url = Math.random() > 0.5 ? TARGET_URL : TARGET_URL + "/gibberish";
-  let res = http.get(url);
+  let url = Math.random() > 0.8 ? TARGET_URL : TARGET_URL + '/gibberish'
+  let res = http.get(url)
 
   check(res, {
-    "Status is ok": (r) => r.status === 200,
-  });
+    'Status is ok': (r) => r.status === 200,
+  })
 
-  sleep(SLEEP);
+  sleep(SLEEP)
 
-  group("Example group", () => {
-    let res = http.get(TARGET_URL);
+  group('Example group', () => {
+    let res = http.get(TARGET_URL)
 
     check(res, {
-      "NOT 500": (r) => r.status !== 500,
-    });
+      'NOT 500': (r) => r.status !== 500,
+    })
 
-    sleep(SLEEP);
-  });
+    sleep(SLEEP)
+  })
 }
