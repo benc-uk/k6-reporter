1fd912bf4ed970e8ef96f8ca47153231c29e8312
Daniel Benages <daniel.benages@wiris.com>
2024-10-14 14:40:48 +0200

    update url filtering for counter methods

--- src/counter.js
+++ src/counter.js
@@ -20,7 +20,7 @@ export class Counters {
     }
 
     urlToCamel = str =>
-        str.toLowerCase().replace(/([/-_.=?][a-zA-Z0-9])/g, group =>
+        str.toLowerCase().replace(/([/-_.=?%][a-zA-Z0-9])/g, group =>
             group
                 .toUpperCase()
                 .replace('-', '')
@@ -29,6 +29,7 @@ export class Counters {
                 .replace('.', '')
                 .replace('=', '')
                 .replace('?', '')
+                .replace('%', '')
         );
     get counterMatrix() {
         return this.counterMatrix
