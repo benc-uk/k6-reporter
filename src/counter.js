ae3c802cbc8342a0a1b3bab09f6a3e62231e010f
Daniel Benages <daniel.benages@wiris.com>
2024-10-14 14:45:08 +0200

    fix regex

--- src/counter.js
+++ src/counter.js
@@ -20,7 +20,7 @@ export class Counters {
     }
 
     urlToCamel = str =>
-        str.toLowerCase().replace(/([/-_.=?%][a-zA-Z0-9])/g, group =>
+        str.toLowerCase().replace(/([\/\-_.=?%][a-zA-Z0-9])/g, group =>
             group
                 .toUpperCase()
                 .replace('-', '')
